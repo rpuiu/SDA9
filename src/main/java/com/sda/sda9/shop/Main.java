f6baaa14d1f2f0b980142c9a36dbb081b5f9684a
raz.puiu <raz.puiu@gmail.com>
2019-05-16 21:38:17 +0300

    Add to shopping cart

--- src/main/java/com/sda/sda9/shop/Main.java
+++ src/main/java/com/sda/sda9/shop/Main.java
@@ -5,9 +5,14 @@ public class Main {
         Product p1 = new Product("milk", 10.3);
         Product p2 = new Product("eggs", 3.3);
 
-        System.out.println(p1.toString());
-        System.out.println(p2.toString());
-
         ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addProduct(p1);
+        shoppingCart.addProduct(p2);
+        shoppingCart.addProduct(p1);
+        shoppingCart.addProduct(p2);
+        shoppingCart.addProduct(p2);
+        shoppingCart.addProduct(p2);
+        shoppingCart.addProduct(p2);
+        shoppingCart.addProduct(p1);
     }
 }
--- src/main/java/com/sda/sda9/shop/ShoppingCart.java
+++ src/main/java/com/sda/sda9/shop/ShoppingCart.java
@@ -3,12 +3,19 @@ package com.sda.sda9.shop;
 public class ShoppingCart {
 
     private Product[] products;
+    private int index;
 
-    public ShoppingCart(){
+    public ShoppingCart() {
+        this.index = 0;
         this.products = new Product[5];
     }
 
-    public void addProduct(Product product){
-
+    public void addProduct(Product product) {
+        if (index < 5) {
+            products[index] = product;
+            index++;
+        } else {
+            System.out.println("Cart is full!!!");
+        }
     }
 }
