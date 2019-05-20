d60e4405989dbb96e1b9cfc20698e6f8b5b1a1f9
raz.puiu <raz.puiu@gmail.com>
2019-05-20 20:02:01 +0300

    Create a method to count vowels inside a string

--- src/main/java/com/sda/sda9/stringuri/VowelCounter.java
+++ src/main/java/com/sda/sda9/stringuri/VowelCounter.java
@@ -2,7 +2,22 @@ package com.sda.sda9.stringuri;
 
 public class VowelCounter {
 
-    static char[] transform(String input){
+    static int count(String s) {
+        int counter = 0;
+        char[] charArray = transform(s);
+        for (char c : charArray) {
+            if (isVowel(c)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    static boolean isVowel(char c) {
+        String vowels = "aeiouAEIOU";
+       return vowels.indexOf(c)!=-1;
+    }
+
+    static char[] transform(String input) {
         return input.toCharArray();
     }
 }
--- src/test/java/com/sda/sda9/stringuri/VowelCounterTest.java
+++ src/test/java/com/sda/sda9/stringuri/VowelCounterTest.java
@@ -7,22 +7,37 @@ import org.junit.Test;
 public class VowelCounterTest {
 
     @Test
-    public void testCharArrayFromString(){
+    public void testCharArrayFromString() {
         String inputString = "String de iasdadfsrdgfknryj";
-       char[] transformedString = VowelCounter.transform(inputString);
-       char expected = 'S';
-       char actual = transformedString[0];
+        char[] transformedString = VowelCounter.transform(inputString);
+        char expected = 'S';
+        char actual = transformedString[0];
+        Assert.assertEquals(expected, actual);
 
-       Assert.assertEquals(expected, actual);
+        Assert.assertNotEquals('s', actual);
 
-       Assert.assertNotEquals('s', actual);
-
-       char actualLastChar = transformedString[transformedString.length-1];
-       Assert.assertEquals('j',actualLastChar);
+        char actualLastChar = transformedString[transformedString.length - 1];
+        Assert.assertEquals('j', actualLastChar);
     }
 
     @Test
-    public void test(){
+    public void test() {
         Assert.assertEquals('S', VowelCounter.transform("String de iasdadfsrdgfknryj")[0]);
+    }
+
+    @Test
+    public void testCounter() {
+        String inputString = "Un String cu vocale";
+        int actual = VowelCounter.count(inputString);
+        int expected = 6;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testIsVowel(){
+        char a = 'a';
+        Assert.assertTrue(VowelCounter.isVowel(a));
+        char z = 'z';
+        Assert.assertFalse(VowelCounter.isVowel(z));
     }
 }
